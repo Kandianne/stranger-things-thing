4e2cb699e1d79095b8e71ee5dd75202c67e4f807
kandiannepierre <kandianne.pierre@timmons.com>
2019-08-26 08:16:59 -0400

    removing test data

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -7,11 +7,11 @@
           </div>
           <div class="col">
             <carousel-3d  :controls-visible="true">
-              <slide v-for="(slide, i) in slides" :key="slide.id" :index="i">
+              <slide v-for="(slide, i) in $t('gallery')" :key="slide.id" :index="i">
                 <figcaption>
                   {{ i + 1 }}
                 </figcaption>
-                <img :src="slide">
+                <img :src="slide.src">
               </slide>
             </carousel-3d>
           </div>
@@ -21,17 +21,7 @@
 
 <script>
 export default {
-  name: 'gallery',
-  data() {
-    return {
-      slides:  [
-        "https://vignette.wikia.nocookie.net/strangerthings8338/images/6/6b/The_Vanishing_of_Will_Byers_S01-E01_SS_005.png/revision/latest/scale-to-width-down/820",
-        "https://www.newsobserver.com/news/local/rjhbpb/picture182497786/alternates/FREE_768/IMG_DISPLAY-ST_201-202_U_2_1_C9CM2NMI_L350297312",
-        "https://nationalpostcom.files.wordpress.com/2017/11/st5.jpg?quality=80&strip=all&w=780&zoom=1",
-        "https://vignette.wikia.nocookie.net/strangerthings8338/images/1/1d/ST-Hawkins-Map.png",
-      ],
-    } 
-  }
+  name: 'gallery'
 }
 </script>
 
